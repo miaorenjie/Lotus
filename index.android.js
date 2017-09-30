f5f20c98b2bbc7b1940846a58ad08c35cfc7ea31
miaorenjie <501216475@qq.com>
2017-09-30 22:11:43 +0800

    简单回忆

---
+++ index.android.js
@@ -0,0 +1,13 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import setup from './js/setup'
+import {
+  AppRegistry,
+} from 'react-native';
+
+
+AppRegistry.registerComponent('Lotus', () => setup);
